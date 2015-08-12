ed6f408858f7366b2fad215f6154575c1f6ff14d
Randy Pensinger <rspensinger@gmail.com>
2015-08-12 12:42:48 -0700

    example: of a main

--- src/test/java/oof/jdbc/PrototypeMain.java
+++ src/test/java/oof/jdbc/PrototypeMain.java
@@ -1,39 +1,66 @@
 package oof.jdbc;
 
+import org.h2.jdbcx.JdbcConnectionPool;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class PrototypeMain {
 
-    private static final ReturnsQuery query = JDBJ.query("tables_by_schema.sql");
-
-    private static final MapQuery<String> QueryReadyToGo = query
-            .map(rs -> rs.getLong("id") + " " + rs.getString("TABLE_SCHEMA") + "." + rs.getString("TABLE_NAME"));
+    private static final MapQuery<Table> QueryReadyToGo = JDBJ
+            .queryString("SELECT ID, TABLE_SCHEMA, TABLE_NAME from information_schema.tables " +
+                    "WHERE TABLE_SCHEMA = :table_schema")
+            .map(Table::from);
 
     public static void main(String[] args) throws Exception {
-        try (final H2DB db = new H2DB("test");
-             final Connection connection = db.getConnection()) {
+        final JdbcConnectionPool db = JdbcConnectionPool.create("jdbc:h2:mem:jdbj_example;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE", "sa", "sa");
+        try (final Connection connection = db.getConnection()) {
+            //can return list
+            final List<Table> results = QueryReadyToGo.toList()
+                    .bindString(":table_schema", "INFORMATION_SCHEMA")
+                    .execute(connection);
+            System.out.println(results);
 
-            //can call arbitrary code against result set
-            query.runnable(rs -> System.out.println(rs.getMetaData().getColumnCount() + " columns")).execute(connection);
-
-            //of course can return list
-            {
-                final List<String> results = QueryReadyToGo.toList()
-                        .bindString(":table_schema", "INFORMATION_SCHEMA")
-                        .execute(connection);
-                System.out.println(results);
-            }
 
             //or use streams, but be sure to close stream
-            try (Stream<String> stream = QueryReadyToGo.stream()
+            try (Stream<Table> stream = QueryReadyToGo.stream()
                     .bindString(":table_schema", "INFORMATION_SCHEMA")
                     .execute(connection)) {
 
-                stream.filter(s->s.length() > 5).forEach(System.out::println);
+                stream.filter(table -> table.id < 0)
+                        .forEach(System.out::println);
             }
+        }finally {
+            db.dispose();
+        }
+    }
 
+    public static class Table {
+
+        public static Table from(ResultSet rs) throws SQLException {
+            return new Table(rs.getLong("id"), rs.getString("table_schema"), rs.getString("table_name"));
+        }
+
+        private final long id;
+        private final String schema;
+        private final String name;
+
+        public Table(long id, String name, String schema) {
+            this.id = id;
+            this.name = name;
+            this.schema = schema;
+        }
+
+        @Override
+        public String toString() {
+            return "Table{" +
+                    "id=" + id +
+                    ", schema='" + schema + '\'' +
+                    ", name='" + name + '\'' +
+                    '}';
         }
     }
 }
