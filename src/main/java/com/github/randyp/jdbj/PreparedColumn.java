3851e15b8afb5ff9e42e3420e3b7e4a932256d48
Randy Pensinger <rspensinger@gmail.com>
2015-08-17 22:06:46 -0700

    organize PreparedColumn methods

--- src/main/java/com/github/randyp/jdbj/PreparedColumn.java
+++ src/main/java/com/github/randyp/jdbj/PreparedColumn.java
@@ -22,16 +22,12 @@ public class PreparedColumn {
         return ps.getConnection().createArrayOf(typeName, elements);
     }
 
-    public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
-        return ps.getConnection().createStruct(typeName, attributes);
+    public Blob createBlob() throws SQLException {
+        return ps.getConnection().createBlob();
     }
 
     public Clob createClob() throws SQLException {
         return ps.getConnection().createClob();
-    }
-
-    public Blob createBlob() throws SQLException {
-        return ps.getConnection().createBlob();
     }
 
     public NClob createNClob() throws SQLException {
@@ -42,12 +38,66 @@ public class PreparedColumn {
         return ps.getConnection().createSQLXML();
     }
 
+    public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
+        return ps.getConnection().createStruct(typeName, attributes);
+    }
+
     public boolean isSet(){
         return set;
     }
 
-    public void setNull(int sqlType) throws SQLException {
-        ps.setNull(parameterIndex, sqlType);
+    public void setArray( Array x) throws SQLException {
+        ps.setArray(parameterIndex, x);
+        set=true;
+    }
+
+    public void setAsciiStream( InputStream x) throws SQLException {
+        ps.setAsciiStream(parameterIndex, x);
+        set=true;
+    }
+
+    public void setAsciiStream( InputStream x, int length) throws SQLException {
+        ps.setAsciiStream(parameterIndex, x, length);
+        set=true;
+    }
+
+    public void setAsciiStream( InputStream x, long length) throws SQLException {
+        ps.setAsciiStream(parameterIndex, x, length);
+        set=true;
+    }
+
+    public void setBigDecimal( BigDecimal x) throws SQLException {
+        ps.setBigDecimal(parameterIndex, x);
+        set=true;
+    }
+
+    public void setBinaryStream( InputStream x) throws SQLException {
+        ps.setBinaryStream(parameterIndex, x);
+        set=true;
+    }
+
+    public void setBinaryStream( InputStream x, int length) throws SQLException {
+        ps.setBinaryStream(parameterIndex, x, length);
+        set=true;
+    }
+
+    public void setBinaryStream( InputStream x, long length) throws SQLException {
+        ps.setBinaryStream(parameterIndex, x, length);
+        set=true;
+    }
+
+    public void setBlob( Blob x) throws SQLException {
+        ps.setBlob(parameterIndex, x);
+        set=true;
+    }
+
+    public void setBlob( InputStream inputStream) throws SQLException {
+        ps.setBlob(parameterIndex, inputStream);
+        set=true;
+    }
+
+    public void setBlob( InputStream inputStream, long length) throws SQLException {
+        ps.setBlob(parameterIndex, inputStream, length);
         set=true;
     }
 
@@ -61,8 +111,58 @@ public class PreparedColumn {
         set=true;
     }
 
-    public void setShort( short x) throws SQLException {
-        ps.setShort(parameterIndex, x);
+    public void setBytes( byte[] x) throws SQLException {
+        ps.setBytes(parameterIndex, x);
+        set=true;
+    }
+
+    public void setCharacterStream( Reader reader) throws SQLException {
+        ps.setCharacterStream(parameterIndex, reader);
+        set=true;
+    }
+
+    public void setCharacterStream( Reader reader, int length) throws SQLException {
+        ps.setCharacterStream(parameterIndex, reader, length);
+        set=true;
+    }
+
+    public void setCharacterStream( Reader reader, long length) throws SQLException {
+        ps.setCharacterStream(parameterIndex, reader, length);
+        set=true;
+    }
+
+    public void setClob( Clob x) throws SQLException {
+        ps.setClob(parameterIndex, x);
+        set=true;
+    }
+
+    public void setClob( Reader reader) throws SQLException {
+        ps.setClob(parameterIndex, reader);
+        set=true;
+    }
+
+    public void setClob( Reader reader, long length) throws SQLException {
+        ps.setClob(parameterIndex, reader, length);
+        set=true;
+    }
+
+    public void setDate( Date x) throws SQLException {
+        ps.setDate(parameterIndex, x);
+        set=true;
+    }
+
+    public void setDate( Date x, Calendar cal) throws SQLException {
+        ps.setDate(parameterIndex, x, cal);
+        set=true;
+    }
+
+    public void setDouble( double x) throws SQLException {
+        ps.setDouble(parameterIndex, x);
+        set=true;
+    }
+
+    public void setFloat( float x) throws SQLException {
+        ps.setFloat(parameterIndex, x);
         set=true;
     }
 
@@ -76,118 +176,8 @@ public class PreparedColumn {
         set=true;
     }
 
-    public void setFloat( float x) throws SQLException {
-        ps.setFloat(parameterIndex, x);
-        set=true;
-    }
-
-    public void setDouble( double x) throws SQLException {
-        ps.setDouble(parameterIndex, x);
-        set=true;
-    }
-
-    public void setBigDecimal( BigDecimal x) throws SQLException {
-        ps.setBigDecimal(parameterIndex, x);
-        set=true;
-    }
-
-    public void setString( String x) throws SQLException {
-        ps.setString(parameterIndex, x);
-        set=true;
-    }
-
-    public void setBytes( byte[] x) throws SQLException {
-        ps.setBytes(parameterIndex, x);
-        set=true;
-    }
-
-    public void setDate( Date x) throws SQLException {
-        ps.setDate(parameterIndex, x);
-        set=true;
-    }
-
-    public void setTime( Time x) throws SQLException {
-        ps.setTime(parameterIndex, x);
-        set=true;
-    }
-
-    public void setTimestamp( Timestamp x) throws SQLException {
-        ps.setTimestamp(parameterIndex, x);
-        set=true;
-    }
-
-    public void setAsciiStream( InputStream x, int length) throws SQLException {
-        ps.setAsciiStream(parameterIndex, x, length);
-        set=true;
-    }
-
-    public void setBinaryStream( InputStream x, int length) throws SQLException {
-        ps.setBinaryStream(parameterIndex, x, length);
-        set=true;
-    }
-
-    public void setObject( Object x, int targetSqlType) throws SQLException {
-        ps.setObject(parameterIndex, x, targetSqlType);
-        set=true;
-    }
-
-    public void setObject( Object x) throws SQLException {
-        ps.setObject(parameterIndex, x);
-        set=true;
-    }
-
-    public void setCharacterStream( Reader reader, int length) throws SQLException {
-        ps.setCharacterStream(parameterIndex, reader, length);
-        set=true;
-    }
-
-    public void setRef( Ref x) throws SQLException {
-        ps.setRef(parameterIndex, x);
-        set=true;
-    }
-
-    public void setBlob( Blob x) throws SQLException {
-        ps.setBlob(parameterIndex, x);
-        set=true;
-    }
-
-    public void setClob( Clob x) throws SQLException {
-        ps.setClob(parameterIndex, x);
-        set=true;
-    }
-
-    public void setArray( Array x) throws SQLException {
-        ps.setArray(parameterIndex, x);
-        set=true;
-    }
-
-    public void setDate( Date x, Calendar cal) throws SQLException {
-        ps.setDate(parameterIndex, x, cal);
-        set=true;
-    }
-
-    public void setTime( Time x, Calendar cal) throws SQLException {
-        ps.setTime(parameterIndex, x, cal);
-        set=true;
-    }
-
-    public void setTimestamp( Timestamp x, Calendar cal) throws SQLException {
-        ps.setTimestamp(parameterIndex, x, cal);
-        set=true;
-    }
-
-    public void setNull( int sqlType, String typeName) throws SQLException {
-        ps.setNull(parameterIndex, sqlType, typeName);
-        set=true;
-    }
-
-    public void setURL( URL x) throws SQLException {
-        ps.setURL(parameterIndex, x);
-        set=true;
-    }
-
-    public void setNString( String value) throws SQLException {
-        ps.setNString(parameterIndex, value);
+    public void setNCharacterStream( Reader value) throws SQLException {
+        ps.setNCharacterStream(parameterIndex, value);
         set=true;
     }
 
@@ -201,13 +191,8 @@ public class PreparedColumn {
         set=true;
     }
 
-    public void setClob( Reader reader, long length) throws SQLException {
-        ps.setClob(parameterIndex, reader, length);
-        set=true;
-    }
-
-    public void setBlob( InputStream inputStream, long length) throws SQLException {
-        ps.setBlob(parameterIndex, inputStream, length);
+    public void setNClob( Reader reader) throws SQLException {
+        ps.setNClob(parameterIndex, reader);
         set=true;
     }
 
@@ -216,8 +201,33 @@ public class PreparedColumn {
         set=true;
     }
 
-    public void setSQLXML( SQLXML xmlObject) throws SQLException {
-        ps.setSQLXML(parameterIndex, xmlObject);
+    public void setNString( String value) throws SQLException {
+        ps.setNString(parameterIndex, value);
+        set=true;
+    }
+
+    public void setNull(int sqlType) throws SQLException {
+        ps.setNull(parameterIndex, sqlType);
+        set=true;
+    }
+
+    public void setNull( int sqlType, String typeName) throws SQLException {
+        ps.setNull(parameterIndex, sqlType, typeName);
+        set=true;
+    }
+
+    public void setObject( Object x) throws SQLException {
+        ps.setObject(parameterIndex, x);
+        set=true;
+    }
+
+    public void setObject( Object x, int targetSqlType) throws SQLException {
+        ps.setObject(parameterIndex, x, targetSqlType);
+        set=true;
+    }
+
+    public void setObject( Object x, SQLType targetSqlType) throws SQLException {
+        ps.setObject(parameterIndex, x, targetSqlType);
         set=true;
     }
 
@@ -226,53 +236,53 @@ public class PreparedColumn {
         set=true;
     }
 
-    public void setAsciiStream( InputStream x, long length) throws SQLException {
-        ps.setAsciiStream(parameterIndex, x, length);
+    public void setObject( Object x, SQLType targetSqlType, int scaleOrLength) throws SQLException {
+        ps.setObject(parameterIndex, x, targetSqlType, scaleOrLength);
         set=true;
     }
 
-    public void setBinaryStream( InputStream x, long length) throws SQLException {
-        ps.setBinaryStream(parameterIndex, x, length);
+    public void setRef( Ref x) throws SQLException {
+        ps.setRef(parameterIndex, x);
         set=true;
     }
 
-    public void setCharacterStream( Reader reader, long length) throws SQLException {
-        ps.setCharacterStream(parameterIndex, reader, length);
+    public void setShort( short x) throws SQLException {
+        ps.setShort(parameterIndex, x);
         set=true;
     }
 
-    public void setAsciiStream( InputStream x) throws SQLException {
-        ps.setAsciiStream(parameterIndex, x);
+    public void setSQLXML( SQLXML xmlObject) throws SQLException {
+        ps.setSQLXML(parameterIndex, xmlObject);
         set=true;
     }
 
-    public void setCharacterStream( Reader reader) throws SQLException {
-        ps.setCharacterStream(parameterIndex, reader);
+    public void setString( String x) throws SQLException {
+        ps.setString(parameterIndex, x);
         set=true;
     }
 
-    public void setBinaryStream( InputStream x) throws SQLException {
-        ps.setBinaryStream(parameterIndex, x);
+    public void setTime( Time x) throws SQLException {
+        ps.setTime(parameterIndex, x);
         set=true;
     }
 
-    public void setNCharacterStream( Reader value) throws SQLException {
-        ps.setNCharacterStream(parameterIndex, value);
+    public void setTime( Time x, Calendar cal) throws SQLException {
+        ps.setTime(parameterIndex, x, cal);
         set=true;
     }
 
-    public void setClob( Reader reader) throws SQLException {
-        ps.setClob(parameterIndex, reader);
+    public void setTimestamp( Timestamp x) throws SQLException {
+        ps.setTimestamp(parameterIndex, x);
         set=true;
     }
 
-    public void setBlob( InputStream inputStream) throws SQLException {
-        ps.setBlob(parameterIndex, inputStream);
+    public void setTimestamp( Timestamp x, Calendar cal) throws SQLException {
+        ps.setTimestamp(parameterIndex, x, cal);
         set=true;
     }
 
-    public void setNClob( Reader reader) throws SQLException {
-        ps.setNClob(parameterIndex, reader);
+    public void setURL( URL x) throws SQLException {
+        ps.setURL(parameterIndex, x);
         set=true;
     }
 
@@ -281,15 +291,5 @@ public class PreparedColumn {
             ps.setObject(parameterIndex, null);
             set = true;
         }
-    }
-
-    public void setObject( Object x, SQLType targetSqlType, int scaleOrLength) throws SQLException {
-        ps.setObject(parameterIndex, x, targetSqlType, scaleOrLength);
-        set=true;
-    }
-
-    public void setObject( Object x, SQLType targetSqlType) throws SQLException {
-        ps.setObject(parameterIndex, x, targetSqlType);
-        set=true;
     }
 }
