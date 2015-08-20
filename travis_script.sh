b4a6525ff01cb058aa82a0a2406777ea831ce25d
Randy Pensinger <rspensinger@gmail.com>
2015-08-19 23:20:19 -0700

    perhaps this will work in travis?

--- travis_script.sh
+++ travis_script.sh
@@ -3,4 +3,5 @@
 set -e
 
 psql -U postgres < src/test/java/com/github/randyp/jdbj/db/postgres_9_4/setup.sql
+mysql -uroot < src/test/java/com/github/randyp/jdbj/db/mysql_5_1/setup.sql
 mvn -B clean test jacoco:report coveralls:report
