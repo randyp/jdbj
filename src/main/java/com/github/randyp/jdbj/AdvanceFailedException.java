8e6ec7d6aa475fa23f4dcc4513d60181e286fa45
Randy Pensinger <rspensinger@gmail.com>
2015-08-23 00:07:49 -0700

    working on docs

--- src/main/java/com/github/randyp/jdbj/AdvanceFailedException.java
+++ src/main/java/com/github/randyp/jdbj/AdvanceFailedException.java
@@ -5,6 +5,7 @@ import java.util.function.Consumer;
 /**
  * Runtime exception which wraps any exception caught in {@link ResultSetSpliterator#tryAdvance(Consumer)}.
  * @see ResultSetSpliterator
+ * @see StreamQuery
  */
 public class AdvanceFailedException extends RuntimeException {
 
