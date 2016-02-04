2e52800e8a3bec8b4fc58a3591c2046a206ee98e
Randy Pensinger <rspensinger@gmail.com>
2016-02-03 18:18:38 -0800

    perhaps this will work on travis ci

--- travis_script.sh
+++ travis_script.sh
@@ -4,4 +4,4 @@ set -e
 
 psql -U postgres < src/test/java/io/codemonastery/jdbj/db/postgres_9_4/setup.sql
 mysql -uroot < src/test/java/io/codemonastery/jdbj/db/mysql_5_1/setup.sql
-mvn -B clean test jacoco:report coveralls:report
+mvn -B -DrepoToken="${coverallstoken}" clean test jacoco:report coveralls:report
