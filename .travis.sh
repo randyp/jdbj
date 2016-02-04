5127f43914458d9f44b56fc15cd8414267bc3c2b
Randy Pensinger <rspensinger@gmail.com>
2016-02-03 21:58:36 -0800

    should separate test and test coverage

--- .travis.sh
+++ .travis.sh
@@ -2,3 +2,4 @@
 
 psql -U postgres < src/test/java/io/codemonastery/jdbj/db/postgres_9_4/setup.sql
 mysql -uroot < src/test/java/io/codemonastery/jdbj/db/mysql_5_1/setup.sql
+mvn -B test
--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "coverallsToken starts with ${coverallsToken:0:4}"
-mvn -DrepoToken="${coverallsToken}" -B clean test jacoco:report coveralls:report
+mvn -DrepoToken="${coverallsToken}" -B jacoco:report coveralls:report
