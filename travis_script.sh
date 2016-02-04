b8af571dbb84737c18b95365be30ddb6740e9b7f
Randy Pensinger <rspensinger@gmail.com>
2016-02-03 18:08:18 -0800

    debugging coveralls integration problem

--- travis_script.sh
+++ travis_script.sh
@@ -4,4 +4,5 @@ set -e
 
 psql -U postgres < src/test/java/io/codemonastery/jdbj/db/postgres_9_4/setup.sql
 mysql -uroot < src/test/java/io/codemonastery/jdbj/db/mysql_5_1/setup.sql
+echo "coveralls ${coverallstoken:0:3}"
 mvn -B clean test jacoco:report coveralls:report
