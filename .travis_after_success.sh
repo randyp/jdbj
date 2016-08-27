b0d191eee4476ec704abbd3172f9caffe07f7b77
Randy Pensinger <rspensinger@gmail.com>
2016-08-27 13:08:20 -0700

    different approach to releasing

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "coverallsToken starts with ${coverallsToken:0:4}"
-mvn -B jacoco:report #coveralls:report
+mvn -B jacoco:report coveralls:report
+
+version_match=`perl -e "print '$TRAVIS_TAG' =~ /^jdbj-\d+\.\d+\.\d+\.\d+$/"`
+if [[ "$version_match" == "1" ]]; then
+    mvn clean deploy --settings .travis/release-settings.xml -DskipTests=true -B
+    curl -X POST --data-urlencode "payload={ \"text\": \"${TRAVIS_TAG} has been released\"}" "${slackrelease}"
+fi
