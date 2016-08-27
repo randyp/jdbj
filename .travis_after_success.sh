2b6b2596651fbc1935f452677e69ec3fd9d15cbc
Randy Pensinger <rspensinger@gmail.com>
2016-08-27 14:30:25 -0700

    try this

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mvn -B jacoco:report coveralls:report
-
 version_match=`perl -e "print '$TRAVIS_TAG' =~ /^jdbj-\d+\.\d+\.\d+\.\d+$/"`
 if [[ "$version_match" == "1" ]]; then
     mvn deploy --settings .travis/release-settings.xml -DskipTests=true -B
     curl -X POST --data-urlencode "payload={ \"text\": \"${TRAVIS_TAG} has been released\"}" "${slackrelease}"
 fi
+
+mvn -B jacoco:report coveralls:report
